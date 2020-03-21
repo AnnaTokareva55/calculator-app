bad4e3b78abf115c5ca462cce59bf062e06fb8b4
AnnaTokareva55 <edelveis2606@yandex.ru>
2020-03-21 12:00:56 +0300

    creating and styling a calculator

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator class="calc" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from "./components/Calculator.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Calculator
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +22,21 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+
+.calc {
+  width: 65%;
+  border: 0.5px solid gray;
+}
+
+.d-flex {
+  display: flex;
+}
+
+.justify-content-end {
+  justify-content: flex-end;
 }
 </style>
+<style src="./App.scss" lang="scss"></style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="calc-wrap">
+    <md-content>
+      <div class="result-field">{{ result || 0 }}</div>
+    </md-content>
+    <div class="d-flex">
+      <md-button class="md-primary cell cell-border">AC</md-button>
+      <md-button class="md-primary cell cell-border">+/-</md-button>
+      <md-button class="md-primary cell cell-border">%</md-button>
+      <md-button class="md-raised md-primary cell">÷</md-button>
+    </div>
+    <div class="d-flex">
+      <md-button class="cell cell-border">7</md-button>
+      <md-button class="cell cell-border">8</md-button>
+      <md-button class="cell cell-border">9</md-button>
+      <md-button class="md-raised md-primary cell">×</md-button>
+    </div>
+    <div class="d-flex">
+      <md-button class="cell cell-border">4</md-button>
+      <md-button class="cell cell-border">5</md-button>
+      <md-button class="cell cell-border">6</md-button>
+      <md-button class="md-raised md-primary cell">-</md-button>
+    </div>
+    <div class="d-flex">
+      <md-button class="cell cell-border">1</md-button>
+      <md-button class="cell cell-border">2</md-button>
+      <md-button class="cell cell-border">3</md-button>
+      <md-button class="md-raised md-primary cell">+</md-button>
+    </div>
+    <div class="d-flex">
+      <md-button class="cell cell-border cell-zero">0</md-button>
+      <md-button class="cell cell-border">,</md-button>
+      <md-button class="md-raised md-primary cell">=</md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data: () => ({
+    result: ""
+  })
+};
+</script>
+
+<style scoped>
+.result-field {
+  padding: 15px;
+  text-align: right;
+  font-size: 38px;
+  background-color: grey;
+  color: white;
+}
+
+.cell {
+  width: 25%;
+  margin: 0;
+  font-size: 18px;
+  background-color: lightgray;
+  border: 0.5px solid gray;
+  border-radius: 0;
+}
+
+.cell-zero {
+  width: 50%;
+}
+</style>
