1e6f08e13904604df859bec4050a9fb53fcefde7
AnnaTokareva55 <edelveis2606@yandex.ru>
2020-03-21 12:37:33 +0300

    method for entering numbers and clearing

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,31 +4,53 @@
       <div class="result-field">{{ result || 0 }}</div>
     </md-content>
     <div class="d-flex">
-      <md-button class="md-primary cell cell-border">AC</md-button>
+      <md-button class="md-primary cell cell-border" @click="allClean"
+        >AC</md-button
+      >
       <md-button class="md-primary cell cell-border">+/-</md-button>
       <md-button class="md-primary cell cell-border">%</md-button>
       <md-button class="md-raised md-primary cell">÷</md-button>
     </div>
     <div class="d-flex">
-      <md-button class="cell cell-border">7</md-button>
-      <md-button class="cell cell-border">8</md-button>
-      <md-button class="cell cell-border">9</md-button>
+      <md-button class="cell cell-border" @click="inputNumber('7')"
+        >7</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('8')"
+        >8</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('9')"
+        >9</md-button
+      >
       <md-button class="md-raised md-primary cell">×</md-button>
     </div>
     <div class="d-flex">
-      <md-button class="cell cell-border">4</md-button>
-      <md-button class="cell cell-border">5</md-button>
-      <md-button class="cell cell-border">6</md-button>
+      <md-button class="cell cell-border" @click="inputNumber('4')"
+        >4</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('5')"
+        >5</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('6')"
+        >6</md-button
+      >
       <md-button class="md-raised md-primary cell">-</md-button>
     </div>
     <div class="d-flex">
-      <md-button class="cell cell-border">1</md-button>
-      <md-button class="cell cell-border">2</md-button>
-      <md-button class="cell cell-border">3</md-button>
+      <md-button class="cell cell-border" @click="inputNumber('1')"
+        >1</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('2')"
+        >2</md-button
+      >
+      <md-button class="cell cell-border" @click="inputNumber('3')"
+        >3</md-button
+      >
       <md-button class="md-raised md-primary cell">+</md-button>
     </div>
     <div class="d-flex">
-      <md-button class="cell cell-border cell-zero">0</md-button>
+      <md-button class="cell cell-border cell-zero" @click="inputNumber('0')"
+        >0</md-button
+      >
       <md-button class="cell cell-border">,</md-button>
       <md-button class="md-raised md-primary cell">=</md-button>
     </div>
@@ -40,7 +62,17 @@ export default {
   name: "Calculator",
   data: () => ({
     result: ""
-  })
+  }),
+  methods: {
+    allClean() {
+      this.result = "";
+    },
+    inputNumber(value) {
+      if (this.result === "0") {
+        this.result = value;
+      } else this.result += value;
+    }
+  }
 };
 </script>
 
